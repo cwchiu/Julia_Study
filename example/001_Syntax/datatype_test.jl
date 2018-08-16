22fec54652d4b449656301d09c83d69419573bf7
Arick <sisimi.pchome@gmail.com>
2018-08-16 16:33:37 +0800

    basic: data type test

--- example/001_Syntax/datatype_test.jl
+++ example/001_Syntax/datatype_test.jl
@@ -34,8 +34,98 @@ const a = 1
     @testset "bool" begin
         @test true == 1
         @test false == 0
+        
+        @test (true | true) == true
+        @test (false | true) == true
+        @test (true | false) == true
+        @test (false | false) == false
+        
+        @test (true & true) == true
+        @test (false & true) == false
+        @test (true & false) == false
+        @test (false & false) == false
     end
 
+    # 類似 NULL
+    @testset "missing" begin
+        @test (missing + 1) === missing
+        @test ("a" * missing) === missing
+        @test abs(missing) === missing
+        @test (missing == 1) === missing
+        @test (missing == missing) === missing
+        @test (missing < 1) === missing
+        
+        @test !(missing === 1)
+
+        @testset "Logical Operations" begin
+            @test (true | missing) 
+            @test (missing | true) 
+            
+            @test (false | missing) === missing
+            @test (missing | false) === missing
+            
+            @test (true & missing) === missing
+            @test (missing & true) === missing
+            
+            @test (false & missing) == false
+            @test (missing & false) == false
+        end
+        
+        @testset "Short-Circuiting Operations" begin
+            @test (true && missing) === missing
+            @test (false && missing) == false
+            
+            @test_throws TypeError (missing || false)
+            @test_throws TypeError (missing && false)
+        end
+        
+        @testset "union missing" begin
+            f(x::Union{Missing, Int}) = if x === missing 
+                0
+            else
+                x*2
+            end
+            
+            @test f(missing) == 0
+            @test f(10) == 20
+        end
+        
+        @testset "skipmissing" begin
+            data = [1, missing]
+            @test sum(data) === missing
+            @test sum(skipmissing(data)) == 1 
+        end
+        
+        @testset "Logical Operations on Arrays" begin
+            @test ([1, missing] == [1, missing]) === missing
+            @test ([1, missing] == [2, missing]) == false
+            @test ([1, 2, missing] == [1, missing, 2]) === missing
+        end
+        
+        @testset "isless" begin
+            @test isless(1, missing)
+            @test isless(Inf, missing)
+            @test !isless(missing, missing)
+        end
+        
+        @testset "isqueal" begin
+            @test !(isequal(missing, 1))
+            @test isequal(missing, missing)
+        
+            @test isequal([1, missing], [1, missing])
+            @test !isequal([1, 2, missing], [1, missing, 2])
+        end
+        
+        @testset "all" begin
+            @test all([true, missing]) === missing
+            @test all([false, missing]) == false
+        end
+        
+        @testset "any" begin
+            @test any([true, missing]) == true
+            @test any([false, missing]) === missing
+        end
+    end
     
     @testset "struct" begin
         d = Dog("yyy")
@@ -73,6 +163,25 @@ const a = 1
         
         d2 = Dict(x => i for (i,x) in enumerate(["one", "two", "three"]))
         @test d2["two"] == 2
+        
+        
+        @test get(d, "two", 99) == 2
+        @test get(d, "none", 99) == 99
+        
+        @test haskey(d, "none") == false
+        @test in(("two"=>2), d)
+        
+        delete!(d, "two")
+        @test haskey(d, "two") == false
+        
+        ret = Set()
+        for (key,val) in d
+            push!(ret, "$(key):$(val)")
+        end
+        @test ret == Set(["one:1", "three:3"])
+        
+        @test merge(Dict("a"=>1, "b"=>2), Dict("b"=>2, "c"=>3)) == Dict("a"=>1, "b"=>2, "c"=>3)
+        
     end
     
     @testset "Set" begin
@@ -105,7 +214,7 @@ const a = 1
         @test [x for x in 1:4 if x % 2 == 0] == [2, 4]
     end
     
-    @testset "Array" begin
+    @testset "Matrix" begin
         @test zeros(Int8, 2, 3) == [0 0 0 ;0 0 0]
         @test ones(Int8, 2, 3) == [1 1 1; 1 1 1]
         @test trues(2, 3) == [true true true; true true true]
@@ -115,6 +224,58 @@ const a = 1
         @test size([0 0 0 ;0 0 0]) == (2,3)
         
         @test size(rand(2,3)) == (2,3)
+        
+        m1 = [
+          1 2
+          3 4
+        ]
+        
+        @test m1' == [
+          1 3
+          2 4
+        ]
+        
+        @test typeof(m1) == Array{Int, 2}
+        @test typeof(Int64[]) == Array{Int64, 1}
+        @test typeof([1,2,3]) == Array{Int,1}
+        
+        a = [1]
+        push!(a, 100)
+        @test a[end] == 100
+        
+        append!(a, 200)
+        @test a[end] == 200
+        
+        @test pop!(a) == 200
+        
+        @test sort!([3,99,1]) == [1,3,99]
+        @test collect(1:3) == [
+         1
+         2
+         3
+        ]
+        
+        m2 = [
+          1 2
+          3 4
+        ]
+        m2[2,2]=100
+        
+        
+        @test m2 == [
+          1 2 
+          3 100
+        ]
+        
+        @test m2.+m2 == [
+          2 4
+          6 200
+        ]
+        
+        @test m2.*m2 == [
+          1 4
+          9 10000
+        ]
     end
     
     @testset "Complex Number" begin
@@ -138,11 +299,16 @@ const a = 1
     end
 
     @testset "data type convert" begin
-        # string to int
-        @test parse(Int8, "1") == 1
-        v = parse(Float64, "3.14")
-        @test v >= 3.14 && v<3.15
-        
+        @testset "string to number" begin
+            @test parse(Int8, "1") == 1
+            v = parse(Float64, "3.14")
+            @test v >= 3.14 && v<3.15
+        end
+
+        @testset "number to string" begin
+            @test string(100) == "100"
+            @test string(3.14) == "3.14"
+        end
     end
 
     @testset "Rational" begin
@@ -160,6 +326,13 @@ const a = 1
         
         f = float(r)
         @test f == 0.2
+    end
+    
+    @testset "number" begin
+        data1 = [1,2,3]
+        @test sum(data1) == 6
+        @test maximum(data1) == 3
+        @test minimum(data1) == 1
     end
 end
 
